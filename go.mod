215d3ef53e753b87c07cded258017efa9a6a0c5d
unknown <seymen544@gmail.com>
2023-04-21 00:44:56 +0200

    change

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bxn0/sem03simpleserver
+
+go 1.20
+
+require (
+	github.com/bxn0/is105sem03 v0.0.0-20230420220032-8da63ae25675 // indirect
+	github.com/uia-worker/is105sem03 v0.0.0-20230416140235-f3824ac768bb // indirect
+)
--- tcp_server.go
+++ tcp_server.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"sync"
+
+	"github.com/bxn0/is105sem03/mycrypt"
 )
 
 func main() {
@@ -37,10 +39,12 @@ func main() {
 						return // fra for løkke
 					}
 					switch msg := string(buf[:n]); msg {
-  				        case "ping":
+					case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
-						_, err = c.Write(buf[:n])
+						dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+						log.Println("Dekrypter melding: ", string(dekryptertMelding))
+						_, err = c.Write([]byte(string(dekryptertMelding)))
 					}
 					if err != nil {
 						if err != io.EOF {
